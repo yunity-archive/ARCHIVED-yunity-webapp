13758555fdb3d8fd9ed954dfbf04c8d5d10d53a3
Flo <goessek@posteo.de>
2015-09-29 18:04:14 +0200

    Removed log from asynchronous basket request

--- src/components/main/controller.js
+++ src/components/main/controller.js
@@ -10,7 +10,6 @@ angular.module('yunity').controller('mainCtrl', [
     $http.get('http://localhost:3000/db').
         then(function(response) {
             $scope.foodbaskets = response.data;  //retrieve results and add to existing results
-            console.log(response);
     });
 
 }]);
