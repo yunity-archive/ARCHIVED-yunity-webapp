8bfc9a06a339dcd1680ac9de3b970b0fbc9f91a5
Flo <goessek@posteo.de>
2015-09-30 13:52:36 +0200

    Added template loader for html files

--- src/_entry.js
+++ src/_entry.js
@@ -1,8 +1,13 @@
 
 require('./assets/less/main.less');
 
+
+
+
+
 require('./app');
 require('./components/about/controller');
 require('./components/main/controller');
 require('./components/map/controller');
 require('./components/search/controller')
+require("../node_modules/angular-strap/src/tab/tab.js");
--- src/components/main/controller.js
+++ src/components/main/controller.js
@@ -1,4 +1,5 @@
 var angular = require('angular');
+var aboutTemplate = require('ngtemplate!./home.html');
 
 angular.module('yunity').controller('mainCtrl', [
     '$scope', '$http', 'foodbasketsPayload',
@@ -8,10 +9,10 @@ angular.module('yunity').controller('mainCtrl', [
     $scope.foodbaskets = foodbasketsPayload;
 
     $scope.tabs = [
-    {title:'Home', content: 'Raw denim you probably haven\'t heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.'},
-    {title:'Profile', content: 'Food truck fixie locavore, accusamus mcsweeney\'s marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.'},
-    {title:'About', content: 'Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney\'s organic lomo retro fanny pack lo-fi farm-to-table readymade.'}
-  ];
+    	{title:'Home', template:aboutTemplate},
+    	{title:'Profile', template:aboutTemplate},
+    	{title:'About', template:aboutTemplate}
+  	];
 
     $http.get('http://localhost:3000/db').
         then(function(response) {
