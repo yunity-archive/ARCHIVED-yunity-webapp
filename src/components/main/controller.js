a7b3b514cf2ff9fc43ff45fd225bad5e236e6135
Neel Peters <njp@informatik.uni-kiel.de>
2015-09-30 13:54:59 +0200

    add searchctrl and results in search tab

--- src/components/main/controller.js
+++ src/components/main/controller.js
@@ -1,21 +1,43 @@
 var angular = require('angular');
+var home = require("html!./home.html");
 
 angular.module('yunity').controller('mainCtrl', [
-    '$scope', '$http', 'foodbasketsPayload',
-    function($scope, $http, foodbasketsPayload) {
+    '$scope','$rootScope', '$http', 'foodbasketsPayload',
+    function ($scope,$rootScope, $http, foodbasketsPayload) {
 
-    // $scope.foodbaskets = [];
-    $scope.foodbaskets = foodbasketsPayload;
+        // $scope.foodbaskets = [];
+        $scope.foodbaskets = foodbasketsPayload;
+        $rootScope.searchResults = [];
 
-    $scope.tabs = [
-    {title:'Home', content: 'Raw denim you probably haven\'t heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.'},
-    {title:'Profile', content: 'Food truck fixie locavore, accusamus mcsweeney\'s marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.'},
-    {title:'About', content: 'Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney\'s organic lomo retro fanny pack lo-fi farm-to-table readymade.'}
-  ];
+        $rootScope.$on("displaySearchResult", function (event, args) {
+            $scope.tabs.activeTab = "Search";
+            $rootScope.searchResults = args.result;
+        });
 
-    $http.get('http://localhost:3000/db').
-        then(function(response) {
-            $scope.foodbaskets = response.data;  //retrieve results and add to existing results
-    });
+        $scope.tabs = [
+            {
+                title: 'Home',
+                content: 'Raw denim you probably haven\'t heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.',
+                template: 'foo.html'
+            },
+            {title: 'Search', template: 'foo.html', content:'content'},
+            {
+                title: 'Profile',
+                content: 'Food truck fixie locavore, accusamus mcsweeney\'s marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.',
+                template: 'foo.html'
+            },
+            {
+                title: 'About',
+                content: 'Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney\'s organic lomo retro fanny pack lo-fi farm-to-table readymade.',
+                template: 'foo.html'
+            }
+        ];
+        $scope.tabs.activeTab = "Home";
 
-}]);
+
+        $http.get('http://localhost:3000/db').
+            then(function (response) {
+                $scope.foodbaskets = response.data;  //retrieve results and add to existing results
+            });
+
+    }]);
--- src/components/search/controller.js
+++ src/components/search/controller.js
@@ -1,16 +1,31 @@
 var angular = require('angular');
 
 
-angular.module('yunity').controller('searchCtrl', ['$scope', '$http',function($scope, $http) {
-	$scope.selectedAddress = '';
+angular.module('yunity').controller('searchCtrl', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
+
+    $scope.list = [];
 
 
+    $scope.submit = function () {
+        if ($scope.searchTag) {
+            var params = {address: $scope.searchTag, sensor: false};
+            return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+                .then(function (res) {
+                    $scope.list = res.data.results;
+                    $rootScope.$broadcast("displaySearchResult", {result: $scope.list});
+                });
 
-	$scope.getSearch = function (viewValue) {
-		var params = {address: viewValue, sensor: false};
-    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
-    .then(function(res) {
-      return res.data.results;
-	});
-  };
-}]);
+        }
+    };
+
+
+        $scope.getSearch = function (viewValue) {
+            var params = {address: viewValue, sensor: false};
+            return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+                .then(function (res) {
+                    return res.data.results;
+                });
+        };
+    }
+    ])
+    ;
