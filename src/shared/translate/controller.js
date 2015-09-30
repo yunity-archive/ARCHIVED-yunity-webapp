69214316f12cc38de0a7949ccd1cc38cf3cb6f69
Flo <goessek@posteo.de>
2015-09-30 19:22:59 +0200

    Refactored to auto translation

--- src/shared/translate/controller.js
+++ src/shared/translate/controller.js
@@ -3,32 +3,46 @@ var angular = require("angular");
 angular.module('yunity').config(function ($translateProvider) {
 	
 	$translateProvider.translations('de_DE', {
-    	APP_HEADLINE:  'Großartige AngularJS App',
-    	NAV_HOME:      'Zur Startseite',
-    	NAV_ABOUT:     'Über',
-    	APP_TEXT:      'Irgendein Text über eine großartige AngularJS App.',
-    	GREETING: 	   'Hallo, mein Name ist {{name}}'
+    	ENGLISH:    "Englisch",
+      GERMAN:     "Deutsch",
+      SWITCH_LANGUAGE:"Sprache wechseln",
+      ABOUT_PAGE: "Über uns",
+      TOGGLE_MAP: "Karte umschalten",
+      SEARCH:     "Suchen",
+      GO:         "Los!",
+      HOME_PAGE:  "Start",
+      SEARCH_PAGE:"Suche",
+      PROFILE:    "Profil",
+      AWESOME:    "Großartig!!",
+      SEARCH_RESULTS:"Suchergebnisse"
 	});
 
 	$translateProvider.translations('en_US', {
-    	APP_HEADLINE:  'Awesome AngularJS App',
-    	NAV_HOME:      'Start',
-    	NAV_ABOUT:     'About',
-    	APP_TEXT:      'Some text about the awesome AngularJS app.',
-    	GREETING: 	   'Hello, my name is {{name}}'
+    	ENGLISH:    "English",
+      GERMAN:     "German", 
+      SWITCH_LANGUAGE:"Switch language",
+      ABOUT_PAGE: "About Page",
+      TOGGLE_MAP: "Toogle Map",
+      SEARCH:     "Search",
+      GO:         "Go!",
+      HOME_PAGE:  "Home",
+      SEARCH_PAGE:"Search",
+      PROFILE:    "Profile",
+      AWESOME:    "Awesome!!",
+      SEARCH_RESULTS:"Search results"
 	});
 
 	$translateProvider.determinePreferredLanguage();
   $translateProvider.useSanitizeValueStrategy('sanitize'); // http://angular-translate.github.io/docs/#/guide/19_security
 });
 
-angular.module('yunity').controller('LangCtrl', function ($scope, $translate) {
+angular.module('yunity').controller('LanguageCtrl', function ($scope, $translate) {
 
   $scope.changeLang = function (key) {
     $translate.use(key).then(function (key) {
-      console.log("Sprache zu " + key + " gewechselt.");
+      console.log("Switched language to " + key + " .");
     }, function (key) {
-      console.log("Irgendwas lief schief.");
+      console.log("Something went wrong!");
     });
   };
 });
