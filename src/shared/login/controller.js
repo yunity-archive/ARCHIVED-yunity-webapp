e76c11e30519640309f45ee4dc2503fb1c930d31
Flo <goessek@posteo.de>
2015-10-02 13:56:51 +0200

    Added remember me functionality to login controller

--- src/shared/login/controller.js
+++ src/shared/login/controller.js
@@ -1,9 +1,18 @@
 import angular from 'angular';
 
+angular.module('yunity').config(function($tooltipProvider) {
+  angular.extend($tooltipProvider.defaults, {
+    html: true
+  });
+})
+
 angular.module('yunity').controller('LoginCtrl', ['$scope', '$yunityAPI', function($scope, $yunityAPI) {
 
-        $scope.submitLoginCredentials = function() {
-            $yunityAPI.authenticate($scope.email, $scope.password);
-        }
+	$scope.rememberMeTooltip = {title: 'Remember me'};
+
+    $scope.submitLoginCredentials = function() {
+    	console.log('Remember Me: ', $scope.credentials.rememberMe);
+        $yunityAPI.authenticate($scope.credentials.email, $scope.credentials.password);
+    }
 
     }]);
