ac4a85d312c9cd36e09d15361259134e5517515a
jsavinc <jsavinc@gmail.com>
2015-10-03 22:06:26 +0200

    Added modal overlay for About page, with a separate about page template in yunity-webapp-common.

--- src/_entry.js
+++ src/_entry.js
@@ -7,5 +7,6 @@ require('./components/main/controller');
 require('./components/map/controller');
 require('./components/search/controller');
 require('./shared/login/controller');
+require('./shared/about/controller');
 require('../node_modules/angular-translate-loader-static-files/angular-translate-loader-static-files');
 //require('../node_modules/yunity-webapp-common/api');
