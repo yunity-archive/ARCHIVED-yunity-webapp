e5414059d3a909b82bc42f7dd1c2e983c4cc49cd
Flo <goessek@posteo.de>
2015-10-03 19:48:19 +0200

    Moved login functionality to directive

--- src/shared/login/controller.js
+++ src/shared/login/controller.js
@@ -1,18 +1,17 @@
 import angular from 'angular';
 
-// angular.module('yunity').config(function($tooltipProvider) {
-//   angular.extend($tooltipProvider.defaults, {
-//     html: true
-//   });
-// })
+angular.module('yunity').directive('login', () => {
+    return {
+        restrict: 'A',
+        templateUrl: 'assets/templates/login.html',
+        controller: function($scope, $yunityAPI)
+        {
+            $scope.rememberMeTooltip = {title: 'Remember me'};
 
-angular.module('yunity').controller('LoginCtrl', ['$scope', '$yunityAPI', function($scope, $yunityAPI) {
-
-	$scope.rememberMeTooltip = {title: 'Remember me'};
-
-    $scope.submitLoginCredentials = function() {
-    	console.log('Remember Me: ', $scope.credentials.rememberMe);
-        $yunityAPI.authenticate($scope.credentials.email, $scope.credentials.password);
+    		$scope.submitLoginCredentials = function() {
+    			console.log('Remember Me: ', $scope.credentials.rememberMe);
+        		$yunityAPI.authenticate($scope.credentials.email, $scope.credentials.password);
+    		}
+        }
     }
-
-    }]);
+});
