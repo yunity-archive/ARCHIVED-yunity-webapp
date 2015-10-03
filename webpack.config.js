022febbdf94e614db260fd7a4fb0bc50f8400c8b
Flo <goessek@posteo.de>
2015-10-03 17:50:22 +0200

    Included yunity-webapp-common (in node_modules) into webpack, so it is built. Removed some comments.

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,14 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(true)
     ],
-    // resolve: {
-    //     extensions: ['', '.js'],
-    //     root: [
-    //         __dirname + '/node_modules'
-    //     ]
-    // },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
-                // TODO(ns): need to include the yunity-webapp-common when it is not npm link'd
-                exclude: /(node_modules|bower_components)/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/yunity-webapp-common'),
+                    path.resolve(__dirname, 'src')
+                ],
                 loaders: ['babel-loader']
             },
             {
