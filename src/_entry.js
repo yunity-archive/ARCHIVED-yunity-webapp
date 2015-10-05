30c3b291c6dbcb5500fc4c28afed5aeae9bf52ec
Neel Peters <njp@informatik.uni-kiel.de>
2015-10-05 12:32:50 +0200

    added sidebar directives templates

--- src/_entry.js
+++ src/_entry.js
@@ -5,6 +5,7 @@ require('./components/about/controller');
 require('./components/main/controller');
 require('./components/map/controller');
 require('./components/search/controller');
+require('./components/sidebar/controller');
 require('./shared/login/controller');
 require('./shared/about/controller');
 require('../node_modules/angular-translate-loader-static-files/angular-translate-loader-static-files');
